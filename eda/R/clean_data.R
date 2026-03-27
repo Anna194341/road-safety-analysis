8b198e812b893236b119d8b4bd8d43f7d633fb91
Anna Sak <ann194341@gmail.com>
2026-03-27 18:57:04 +0200

    cleaning data

---
+++ eda/R/clean_data.R
@@ -0,0 +1,40 @@
+library(readr)
+library(dplyr)
+
+df <- read_csv("data/processed/collisions_2024_decoded.csv", show_col_types = FALSE)
+
+missing_values <- c(
+  "Unknown",
+  "unknown (self reported)",
+  "Data missing or out of range",
+  "Unallocated"
+)
+
+char_cols <- names(df)[sapply(df, is.character)]
+df[char_cols] <- lapply(df[char_cols], function(x) {
+  x[x %in% missing_values] <- NA
+  x
+})
+
+df$enhanced_severity_collision[df$enhanced_severity_collision == -1] <- NA
+
+df$enhanced_severity_collision <- as.character(df$enhanced_severity_collision)
+
+df$date <- as.Date(df$date, format = "%d/%m/%Y")
+
+cols_to_remove <- c(
+  "collision_index",
+  "collision_ref_no",
+  "collision_year",
+  "local_authority_highway_current",
+  "local_authority_district"
+)
+
+df <- df[, !(names(df) %in% cols_to_remove)]
+
+na_summary <- colSums(is.na(df))
+print(na_summary[na_summary > 0])
+
+glimpse(df)
+
+write_csv(df, "data/processed/collisions_2024_cleaned.csv")
